2cf67b4bb5ce26b038c8c196f9abcaa8a721d348
corentin5002 <csabier@orange.fr>
2025-01-27 12:07:57 +0100

    Init Emotion Detection project

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Streamlit Interface")
+
+st.write("This is a simple interface for Streamlit.")
---
+++ command-launch.sh
@@ -0,0 +1,4 @@
+streamlit run app.py
+
+# Or for pycharm config
+# python -m streamlit run your_script.py
